e91acc943c564701e36d767faddcd11a286992cc
shatun26 <emagrof20@gmail.com>
2021-03-10 21:23:02 +0300

    Create video player

--- script.js
+++ script.js
@@ -9,9 +9,23 @@ const swiper = new Swiper(".swiper-container", {
   },
 
   navigation: {
-    nextEl: '.swiper-next',
-    prevEl: '.swiper-prev',
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
   },
 });
 
-
+let video = document.querySelector(".Video video");
+let cVideo = document.querySelector('.c-video')
+let psevdoPlay = document.querySelector(".psevdo-play");
+function togglePlayPause() {
+  if (video.paused) {
+    video.play();
+    psevdoPlay.style.opacity = 0;
+  } else {
+    video.pause();
+    psevdoPlay.style.opacity = 1;
+  }
+}
+cVideo.onclick = function () {
+  togglePlayPause();
+};
