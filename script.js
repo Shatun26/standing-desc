720c44f55a4388e9fc9141a55f4377c861b4fabc
Shatun26 <emagrof20@gmail.com>
2021-03-15 21:09:12 +0300

    Complete burger

--- script.js
+++ script.js
@@ -30,4 +30,8 @@ psevdoPlay.onclick = function(){
 document.querySelector('.burger').addEventListener('click', ()=>{
   document.querySelector('.burger').classList.toggle('active')
   document.querySelector('.header_icons').classList.toggle('active')
+  document.querySelector('.logo').classList.toggle('active')
+
+  document.querySelector('.header_icons').classList.toggle('disable')
+
 })
